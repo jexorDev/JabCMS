c8cf970ff3d3ed2b8138cef1b603d2b675d1b4c6
Dustin Seger <seger.dustin@gmail.com>
2016-01-10 01:57:16 -0600

    prettied up more of the post index page.  broke stuff on create post page

--- JabCMS/Models/Post.cs
+++ JabCMS/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,14 @@ namespace JabCMS.Models
     public class Post
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="Please enter a title.")]
         public string Title { get; set; }
         public Author Author { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DateCreated { get; set; }
+
         public string Content { get; set; }
 
         public virtual ICollection<Category> Categories { get; set; }
